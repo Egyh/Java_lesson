7de3cc73850be560e6ed32ad5d72a6ae1320874b
Egyh <124222570+Egyh@users.noreply.github.com>
2023-06-29 04:41:43 +0000

    メッソド定義

---
+++ lesson3.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    // それぞれ年齢に関する引数を追加
+    printData("Kate Jones", 27);
+    printData("John Christopher Smith", 65);
+  }
+
+  // 年齢に関する引数
+    System.out.println("私の名前は" + name + "です");
+    // 「年齢は◯◯歳です」と出力し
+    System.out.println("年齢は" + age + "歳です");
+    
+  }
+}
